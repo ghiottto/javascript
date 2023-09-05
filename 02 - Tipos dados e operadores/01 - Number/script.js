503a57714dc47c384636347c8410873c093bb0e6
ghiottto <91765101+ghiottto@users.noreply.github.com>
2023-09-05 13:55:14 -0300

    Initial commit

---
+++ 02 - Tipos dados e operadores/01 - Number/script.js
@@ -0,0 +1,3 @@
+console.log(typeof 7);
+console.log(typeof -9);
+console.log(typeof 124.5);
---
+++ 02 - Tipos dados e operadores/05 - Strings Avançadas/script.js
@@ -0,0 +1,6 @@
+console.log("Essa é uma String \n De duas linhas");
+console.log('O meu nome é "Jorge"');
+console.log("O meu nome é 'Carla'");
+console.log(`A soma de 2 + 2 é ${2 + 2}`);
+console.log(`A soma de 2 * 5 é ${2 * 5}`);
+console.log('Carlos ' + 'e ' + 'Jorge ' + 'foram a cidade comprar leite.');
