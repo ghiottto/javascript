bf27161da3aac2f3948db1b3fd2ef9e5622281f5
ghiottto <91765101+ghiottto@users.noreply.github.com>
2023-09-12 19:33:42 -0300

    Novos exercícios

---
+++ 04 - Variáveis/01 - Variáveis/script.js
@@ -0,0 +1,23 @@
+let nome = 'Ghiotto';
+
+console.log(nome);
+console.log(`Meu nome é ${nome}`);
+
+let laranjas = 5;
+laranjas = laranjas + 1;
+
+console.log(laranjas);
+console.log(laranjas * laranjas);
+console.log(`Temos ${laranjas} laranjas na sexta`);
+
+nome = 'João';
+
+console.log(nome);
+
+laranjas = 942;
+
+console.log(laranjas);
+
+let um = 1, dois = 2, tres = 3;
+
+console.log(um + dois + tres);
