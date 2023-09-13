824812ed6c9e4c450e23c41793ed0857504192f8
ghiottto <91765101+ghiottto@users.noreply.github.com>
2023-09-13 15:58:32 -0300

    Novos documentos

---
+++ 04 - Variáveis/03 - Convenção/Script.js
@@ -0,0 +1,10 @@
+let nome3 = 'teste';
+let $nome = 'teste2';
+let _nome = 'teste3';
+let Nome = 'teste4';
+let meuNome = 'teste5';
+let meu_nome = 'teste6';
+
+// let !nome = 'teste1';
+// let @nome = 'teste2';
+// let 3nome = 'teste3';
