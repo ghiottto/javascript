ef6402d44e411b2867adb8e77e3f7fa6e754e85d
ghiottto <91765101+ghiottto@users.noreply.github.com>
2023-09-25 22:45:22 -0300

    ex08

---
+++ 04 - Variáveis/07 - Math/Script.js
@@ -0,0 +1,15 @@
+let maior = Math.max(6, 12, 534, 1092, 5);
+
+console.log(maior);
+
+let menor = Math.min(4, 2, 5, 12);
+
+console.log(menor);
+
+let arredondar = Math.round(5.234321);
+
+console.log(arredondar);
+
+let arredondarParaCima = Math.ceil(6.24242455);
+
+console.log(arredondarParaCima);
